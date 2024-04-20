6f051d2390626c0bb469c89fb587d613d4a83e71
Mohammad ALwassy <153779428+Alwassy-M@users.noreply.github.com>
2024-04-20 16:00:54 +0200

    Add files via upload

---
+++ checkpoint_4.py
@@ -0,0 +1,41 @@
+import math
+
+#Exercise 1: Create a list, tuple, float, integer, decimal, and dictionary.
+
+list = ['wali', 'one', 'hello']
+tuple = ('Item_1',123, False, 0.0001,[1, 'two'])
+my_float = 100.222
+my_int = 1993
+my_deci = 10.10
+dictionary = {
+  'first_name': 'Angel',
+  'last_name': 'Kindel',
+  'age': '1000'
+}
+
+#Exercise 2: Round your float up.
+print(round(my_float))
+
+#Exercise 3: Get the square root of your float.
+print(math.sqrt(my_float))
+
+#Exercise 4: Select the first element from your dictionary.
+print(dictionary['first_name'])
+
+#Exercise 5: Select the second element from your tuple.
+print(tuple[0])
+
+#Exercise 6: Add an element to the end of your list.
+list.append('added_element')
+print(list)
+
+#Exercise 7: Replace the first element in your list.
+list[0]= 'replaced'
+print(list)
+
+#Exercise 8: Sort your list alphabetically.
+print(sorted(list))
+
+#Exercise 9: Use reassignment to add an element to your tuple.
+tuple = tuple + ('adding_another_element',)
+print(tuple)
